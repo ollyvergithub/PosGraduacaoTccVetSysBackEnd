f0eb15885855e24b8563a39be3b099c188300604
ollyvergithub <ollyverottoboni@gmail.com>
2023-03-26 11:07:14 -0300

    Altera SocialAccountAdapter método populate_user

--- vet_sys/users/adapters.py
+++ vet_sys/users/adapters.py
@@ -1,7 +1,9 @@
 from typing import Any
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_email, user_field, user_username
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.utils import valid_email_or_none
 from django.conf import settings
 from django.http import HttpRequest
 
@@ -14,3 +16,20 @@ class AccountAdapter(DefaultAccountAdapter):
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+    def populate_user(self, request, sociallogin, data):
+        username = data.get('username')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        name = data.get('name')
+        user = sociallogin.user
+        emailtouname = email.split('@')[0] + "_" + email.split('@')[1].split('.')[0]
+
+        user_email(user, valid_email_or_none(email) or '')
+        name_parts = (name or '').partition(' ')
+        user_field(user, 'first_name', first_name or name_parts[0])
+        user_field(user, 'last_name', last_name or name_parts[2])
+        user_username(user, username or emailtouname)
+        return user
+
