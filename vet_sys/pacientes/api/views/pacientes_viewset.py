5110a8279965bd9fb567d2555021a194df01553a
ollyvergithub <ollyverottoboni@gmail.com>
2023-04-14 14:02:08 -0300

    Inicia a implementação de Geração de Relatório - Pacientes

--- vet_sys/pacientes/api/views/pacientes_viewset.py
+++ vet_sys/pacientes/api/views/pacientes_viewset.py
@@ -1,16 +1,23 @@
+from datetime import date
+
+from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 from ..serializers.paciente_serializer import PacienteSerializer
 from ...models import Paciente
+from django.template.loader import get_template
+from django.template.loader import render_to_string
+from weasyprint import HTML
 
 
 class PacientesViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
     lookup_field = 'uuid'
-    queryset = Paciente.objects.all().order_by('id',)
+    queryset = Paciente.objects.all().order_by('id', )
     serializer_class = PacienteSerializer
 
     def get_queryset(self):
@@ -32,3 +39,28 @@ class PacientesViewSet(viewsets.ModelViewSet):
             qs = qs.filter(raca__uuid=raca_uuid)
 
         return qs
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='gerar-relatorio-pdf',
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[TokenAuthentication]
+    )
+    def gerar_relatorio_pdf(self, request):
+
+        data_atual = date.today().strftime("%d-%m-%Y")
+        usuario_logado = self.request.user
+
+        html_string = render_to_string(
+            'pdf/pacientes/pdf.html', {'usuario_logado': usuario_logado, 'data_atual': data_atual}).encode(encoding="UTF-8")
+
+        html_pdf = HTML(string=html_string, base_url=self.request.build_absolute_uri()).write_pdf()
+
+        response = HttpResponse(
+            html_pdf,
+            content_type='application/pdf;'
+        )
+        response['Content-Disposition'] = 'filename="relatorio-pacientes.pdf"'
+
+        return response
