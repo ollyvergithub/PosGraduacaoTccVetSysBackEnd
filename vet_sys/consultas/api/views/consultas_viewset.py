945019263dd7a30af68386bfd02a0097b2f4ddc6
ollyvergithub <ollyverottoboni@gmail.com>
2023-04-29 16:05:44 -0300

    Implementa Estatísticas Consultas

--- vet_sys/consultas/api/views/consultas_viewset.py
+++ vet_sys/consultas/api/views/consultas_viewset.py
@@ -14,6 +14,10 @@ from rest_framework.decorators import action
 from django.template.loader import render_to_string
 from weasyprint import HTML
 from ..serializer.consulta_serializer import ConsultaSerializer, ConsultaHistoricoDeConsultasSerializer
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.ticker import MaxNLocator
 
 
 class ConsultasViewSet(viewsets.ModelViewSet):
@@ -87,7 +91,8 @@ class ConsultasViewSet(viewsets.ModelViewSet):
         authentication_classes=[TokenAuthentication]
     )
     def historico_de_consultas_por_paciente(self, request):
-        from vet_sys.consultas.api.serializer.validation_serializers.consulta_validate_serializer import ConsultaPacienteValidateSerializer
+        from vet_sys.consultas.api.serializer.validation_serializers.consulta_validate_serializer import \
+            ConsultaPacienteValidateSerializer
 
         query = ConsultaPacienteValidateSerializer(data=self.request.query_params)
         query.is_valid(raise_exception=True)
@@ -98,11 +103,109 @@ class ConsultasViewSet(viewsets.ModelViewSet):
         paciente = Paciente.objects.get(uuid=paciente_uuid)
 
         if consulta_uuid:
-            consultas = Consulta.objects.filter(paciente=paciente).order_by('-data_da_consulta').exclude(uuid=consulta_uuid)
+            consultas = Consulta.objects.filter(paciente=paciente).order_by('-data_da_consulta').exclude(
+                uuid=consulta_uuid)
         else:
             consultas = Consulta.objects.filter(paciente=paciente).order_by('-data_da_consulta')
 
         return Response(ConsultaHistoricoDeConsultasSerializer(consultas, many=True).data)
 
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='gerar-estatisticas-por-paciente-ano',
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[TokenAuthentication]
+    )
+    def gerar_estatisticas_por_paciente_ano(self, request):
+        ano = request.query_params.get('ano')
+        paciente_uuid = request.query_params.get('paciente')
 
+        paciente = Paciente.objects.get(uuid=paciente_uuid)
 
+        quantidade_de_cadastros_por_mes = []
+        for i in range(1, 13):
+            result = Consulta.objects.filter(criado_em__month=i, criado_em__year=ano, paciente=paciente).count()
+            quantidade_de_cadastros_por_mes.append(result)
+
+        fig, ax = plt.subplots()
+
+        # Convertendo valores eixo y (Quantidade para inteiro)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+        ax.bar(meses, quantidade_de_cadastros_por_mes)
+
+        ax.set(xlabel='Meses', ylabel='Quantidade', title=f'Número de consultas por mês de {paciente.nome} em {ano}')
+        ax.grid()
+
+        response = HttpResponse(content_type='image/png')
+        canvas = FigureCanvasAgg(fig)
+        canvas.print_png(response)
+        matplotlib.pyplot.close()
+        return response
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='gerar-estatisticas-por-ano',
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[TokenAuthentication]
+    )
+    def gerar_estatisticas_por_ano(self, request):
+        ano = request.query_params.get('ano')
+
+        quantidade_de_cadastros_por_mes = []
+        for i in range(1, 13):
+            result = Consulta.objects.filter(criado_em__month=i, criado_em__year=ano).count()
+            quantidade_de_cadastros_por_mes.append(result)
+
+        fig, ax = plt.subplots()
+
+        # Convertendo valores eixo y (Quantidade para inteiro)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+        ax.bar(meses, quantidade_de_cadastros_por_mes)
+
+        ax.set(xlabel='Meses', ylabel='Quantidade', title=f'Número de consultas por mês em {ano}')
+        ax.grid()
+
+        response = HttpResponse(content_type='image/png')
+        canvas = FigureCanvasAgg(fig)
+        canvas.print_png(response)
+        matplotlib.pyplot.close()
+        return response
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='gerar-estatisticas-por-mes-e-ano',
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[TokenAuthentication]
+    )
+    def gerar_estatisticas_por_mes_e_ano(self, request):
+
+        ano = request.query_params.get('ano')
+        mes = request.query_params.get('mes')
+
+        result = Consulta.objects.filter(criado_em__month=mes, criado_em__year=ano).count()
+
+        fig, ax = plt.subplots()
+
+        # Convertendo valores eixo y (Quantidade para inteiro)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        meses = {"01": "Jan", "02": "Fev", "03": "Mar", "04": "Abr", "05": "Mai", "06": "Jun", "07": "Jul", "08": "Ago",
+                 "09": "Set", "10": "Out", "11": "Nov", "12": "Dez"}
+
+        ax.bar(meses[mes], result)
+
+        ax.set(xlabel='Mes', ylabel='Quantidade', title=f"Número de consultas em {meses[mes]} de {ano}")
+        ax.grid()
+
+        response = HttpResponse(content_type='image/png')
+        canvas = FigureCanvasAgg(fig)
+        canvas.print_png(response)
+        matplotlib.pyplot.close()
+        return response
